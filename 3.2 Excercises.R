27b2b79cd58beee271e50d0b20f1676c081c4e47
ted-orourke <torourke97@gmail.com>
2016-06-01 19:26:47 +0200

    Third Acticity - filter out the NA onsettimes

--- 3.2 Excercises.R
+++ 3.2 Excercises.R
@@ -2,3 +2,10 @@ library(readr)
 download.file("https://raw.githubusercontent.com/ICI3D/RTutorials/master/OswegoTutorial.csv", "diet.csv")
 diet <- read_csv("diet.csv")
 diet$age <- strtoi(diet$age)
+
+?filter
+male <- filter(diet, diet$sex == 1)
+male$sex <- "Male"
+female <- filter(diet, diet$sex == 1)
+female$sex <- "Female"
+diet <- rbind(male, female)
